e5d2c489e7d73609d4940ce8c7e01c80e6ed86d9
Diego Zazueta <Diegoztag@gmail.com>
2025-02-19 01:01:06 -0700

    Commit inicial: se agrega plantilla base de springboot con maven y dependencias minimas necesarias

---
+++ src/main/java/com/coppel/crud_polizas/CrudPolizasApplication.java
@@ -0,0 +1,13 @@
+package com.coppel.crud_polizas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPolizasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPolizasApplication.class, args);
+	}
+
+}
